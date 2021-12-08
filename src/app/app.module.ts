4bc875f76f7a8f3e5f57d4b70a30ce8a3e8f5ec6
Ary Demitropulos <demitropulosary@gmail.com>
2021-12-08 20:52:54 -0300

    Se migra logica de error a componente input-error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+
+import { HomeComponent } from './views/home/home.component';
+import { CartComponent } from './views/cart/cart.component';
+import { AuthenticationComponent } from './views/authentication/authentication.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { MovieComponent } from './components/movie/movie.component';
-import { AuthenticationComponent } from './views/authentication/authentication.component';
-import { HomeComponent } from './views/home/home.component';
-import { CartComponent } from './views/cart/cart.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { CartIconComponent } from './components/cart-icon/cart-icon.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+import { InputErrorComponent } from './components/input-error/input-error.component';
 
 @NgModule({
   declarations: [
@@ -29,12 +34,14 @@ import { AngularMaterialModule } from './modules/angular-material/angular-materi
     CartComponent,
     MovieDetailsComponent,
     CartIconComponent,
+    InputErrorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
   hidePassword: boolean = true;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      user: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {}
+
+  get user() {
+    return this.form.get('user')! as FormControl;
+  }
+
+  get password() {
+    return this.form.get('password')! as FormControl;
+  }
 }
